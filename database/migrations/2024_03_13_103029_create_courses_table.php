620fcb7559ceea2b9a43c09966b407e24b177bdd
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-03-13 14:52:58 +0300

    complete homepage test

--- database/migrations/2024_03_13_103029_create_courses_table.php
+++ database/migrations/2024_03_13_103029_create_courses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->timestamp('released_at')->nullable();
             $table->timestamps();
         });
     }
