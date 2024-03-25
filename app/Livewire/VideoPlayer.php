3c70f7f68370025b510b6ad9316cabf78c7f470e
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-03-25 17:09:29 +0300

    feat: complete list course videos

--- app/Livewire/VideoPlayer.php
+++ app/Livewire/VideoPlayer.php
@@ -9,6 +9,13 @@ class VideoPlayer extends Component
 {
     public $video;
 
+    public $courseVideos;
+
+    public function mount()
+    {
+        $this->courseVideos = $this->video->course->videos;
+    }
+
     public function render(): View
     {
         return view('livewire.video-player');
